f33a59fda6f54b4973dc715e821771cd74ee334c
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-08-06 14:45:25 +0200

    Add auth (#22)

--- test/messages.spec.js
+++ test/messages.spec.js
@@ -2,15 +2,6 @@ import assert from 'assert';
 
 import * as messages from '../lib/messages';
 
-describe('auth message', () => {
-  it('should contain api password', () => {
-    assert.deepEqual(messages.auth('hello'), {
-      type: 'auth',
-      api_password: 'hello',
-    });
-  });
-});
-
 describe('auth access token message', () => {
   it('should contain access token', () => {
     assert.deepEqual(messages.authAccessToken('hello'), {
