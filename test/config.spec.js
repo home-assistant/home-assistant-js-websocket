6332820f060199686f2f4959151d1523029eec51
Paulus Schoutsen <balloob@gmail.com>
2018-08-07 18:26:40 +0200

    Refactor subscriptions

---
+++ test/config.spec.js
@@ -0,0 +1,51 @@
+import assert from 'assert';
+
+import subscribeConfig from '../lib/config';
+import { mockConnection, createAwaitableEvent } from './util';
+
+const MOCK_CONFIG = {
+  hello: 'bla',
+  components: ['frontend']
+};
+
+describe('subscribeConfig', () => {
+  let conn;
+  let awaitableEvent;
+
+  beforeEach(() => {
+    conn = mockConnection();
+    conn.getConfig = async () => MOCK_CONFIG;
+    awaitableEvent = createAwaitableEvent();
+  });
+
+  it('should load initial config', async () => {
+    awaitableEvent.prime();
+    subscribeConfig(conn, awaitableEvent.set);
+
+    const config = await awaitableEvent.wait();
+
+    assert.deepStrictEqual(config, MOCK_CONFIG);
+  });
+
+  it('should handle component loaded events', async () => {
+    subscribeConfig(conn, awaitableEvent.set);
+
+    // We need to sleep to have it process the first full load
+    await 0;
+
+    awaitableEvent.prime();
+
+    conn.mockEvent('component_loaded', {
+      data: {
+        component: 'api'
+      }
+    });
+
+    const config = await awaitableEvent.wait();
+
+    assert.deepEqual(config, {
+      hello: 'bla',
+      components: ['frontend', 'api'],
+    });
+  });
+});
