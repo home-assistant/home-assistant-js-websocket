92ee5b463498ec6cc92bd0ef08eb4fa6fc91c8a6
Paulus Schoutsen <balloob@gmail.com>
2018-08-28 14:39:43 +0200

    Fix tests

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -5,13 +5,13 @@ import { Auth } from "../lib/auth";
 describe("Auth", () => {
   it("should indicate correctly when token expired", () => {
     const auth = new Auth({
-      expires: Date.now()
+      expires: Date.now() - 1000
     });
     assert.strictEqual(auth.expired, true);
   });
   it("should indicate correctly when token not expired", () => {
     const auth = new Auth({
-      expires: Date.now() + 20000
+      expires: Date.now() + 1000
     });
     assert.strictEqual(auth.expired, false);
   });
