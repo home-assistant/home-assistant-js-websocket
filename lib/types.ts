df0b721557f28135d90fe62e32d0abf4399cc2db
Bram Kragten <mail@bramkragten.nl>
2020-05-28 16:22:03 +0200

    Add state to HassConfig

--- lib/types.ts
+++ lib/types.ts
@@ -58,6 +58,7 @@ export type HassConfig = {
   version: string;
   config_source: string;
   safe_mode: boolean;
+  state: "NOT_RUNNING" | "STARTING" | "RUNNING" | "STOPPING" | "FINAL_WRITE";
   external_url: string | null;
   internal_url: string | null;
 };
