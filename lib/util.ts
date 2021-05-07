ce668144e6abaf668354cc4338170f7f89eea2c1
Bram Kragten <mail@bramkragten.nl>
2021-05-07 21:16:21 +0200

    Get service descriptions when a new service is registered (#151)

--- lib/util.ts
+++ lib/util.ts
@@ -9,3 +9,35 @@ export function parseQuery<T>(queryString: string) {
   }
   return query as T;
 }
+
+// From: https://davidwalsh.name/javascript-debounce-function
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+// eslint-disable-next-line: ban-types
+export const debounce = <T extends (...args: any[]) => unknown>(
+  func: T,
+  wait: number,
+  immediate = false
+): T => {
+  let timeout: number | undefined;
+  // @ts-ignore
+  return function (...args) {
+    // @ts-ignore
+    const context = this;
+    const later = () => {
+      timeout = undefined;
+      if (!immediate) {
+        func.apply(context, args);
+      }
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) {
+      func.apply(context, args);
+    }
+  };
+};
